1599219849cdab19112fd9458cc9cdc2d7e216f9
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-05-10 13:28:44 +0200

    Basic gameplay

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { currentWord } from "./lib/currentWord.ts";
+import { type Evaluation, newGame } from "./lib/game.ts";
 
-console.log(currentWord());
+const game = ref(newGame(currentWord()));
+const guesses = ref<Evaluation[]>([]);
+const input = ref("");
+
+const submit = (e: SubmitEvent) => {
+  e.preventDefault();
+  const guess = game.value.guess(input.value);
+  if (guess.error) {
+    alert(guess.msg);
+    return;
+  }
+  guesses.value.push(guess.evaluation);
+};
 </script>
 
 <template>
-  <div>Hello, world!</div>
+  <form @submit="submit">
+    <input class="border" type="text" v-model="input" />
+    <button>Submit</button>
+  </form>
+  <div v-for="guess in guesses">
+    <div class="font-mono">
+      <span
+        :class="
+          letter.color === 'yellow' ? 'bg-amber-500'
+          : letter.color === 'green' ? 'bg-lime-500'
+          : 'bg-stone-500'
+        "
+        v-for="letter in guess.letters"
+        >{{ letter.letter }}</span
+      >
+      {{ guess.gameState }}
+    </div>
+  </div>
 </template>
---
+++ src/lib/game.ts
@@ -0,0 +1,67 @@
+export type Evaluation = {
+  letters: ColoredLetter[];
+  gameState: GameState;
+};
+export type ColoredLetter = {
+  letter: string;
+  color: LetterColor;
+};
+export type LetterColor = "gray" | "green" | "yellow";
+export type GameState = "ongoing" | "success" | "fail";
+
+const WORD_LENGTH = 5;
+const MAX_GUESSES = 6;
+
+export const newGame = (word: string) => {
+  let gameState: GameState = "ongoing";
+  const evaluations: Evaluation[] = [];
+  return {
+    guess: (
+      guess: string,
+    ):
+      | { error: true; msg: string }
+      | { error: false; evaluation: Evaluation } => {
+      if (gameState !== "ongoing") {
+        return { error: true, msg: "Game is over" };
+      }
+      if (guess.length < WORD_LENGTH) {
+        return { error: true, msg: "Word too short" };
+      }
+      if (guess.length > WORD_LENGTH) {
+        return { error: true, msg: "Word too long" };
+      }
+      const colors = new Map<number, LetterColor>();
+      const wordArray = [...word];
+      const guessArray = [...guess];
+      wordArray.forEach((wordLetter, i) => {
+        if (wordLetter === guessArray[i]) {
+          colors.set(i, "green");
+        }
+      });
+      const wordCandidates = wordArray.filter(
+        (_word, i) => colors.get(i) !== "green",
+      );
+      guessArray.forEach((guessLetter, i) => {
+        if (colors.has(i)) return;
+        console.log(guessLetter, wordCandidates);
+        const wordIndex = wordCandidates.indexOf(guessLetter);
+        if (wordIndex === -1) return;
+        wordCandidates.splice(wordIndex, 1);
+        colors.set(i, "yellow");
+      });
+      const letters = guessArray.map(
+        (guessLetter, i): ColoredLetter => ({
+          letter: guessLetter,
+          color: colors.get(i) || "gray",
+        }),
+      );
+      gameState =
+        guess === word ? "success"
+        : evaluations.length + 1 === MAX_GUESSES ? "fail"
+        : "ongoing";
+      const evaluation: Evaluation = { letters, gameState };
+      evaluations.push(evaluation);
+      return { error: false, evaluation };
+    },
+  };
+};
