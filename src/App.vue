30e52eb4410df9bcfcea58fd4333d92102b6e017
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-05-10 11:29:33 +0200

    Getting the daily word

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { currentWord } from "./lib/currentWord.ts";
+
+console.log(currentWord());
+</script>
+
 <template>
   <div>Hello, world!</div>
 </template>
---
+++ src/lib/currentWord.ts
@@ -0,0 +1,45 @@
+import data from "./data.json";
+
+const SEED = 1778404414407;
+const NOW = new Date();
+
+const isISOToday = (date: Date) =>
+  date.getUTCDate() === NOW.getUTCDate() &&
+  date.getUTCMonth() === NOW.getUTCMonth() &&
+  date.getUTCDate() === NOW.getUTCDate();
+
+export const currentWord = () => {
+  const prng = splitmix32(SEED);
+  const date = new Date(data.start);
+  const allWords: string[] = [];
+  const words: string[] = [];
+  while (true) {
+    const newWords = data.words[date.toISOString() as keyof typeof data.words];
+    if (newWords) {
+      allWords.push(...newWords);
+      words.push(...newWords);
+    }
+    if (words.length === 0) {
+      words.push(...allWords);
+    }
+    const i = Math.floor(prng() * words.length);
+    if (isISOToday(date)) {
+      return words[i];
+    }
+    words.splice(i, 1);
+    date.setUTCDate(date.getUTCDate() + 1);
+  }
+};
+
+// Seedable PRNG, see https://stackoverflow.com/a/47593316
+const splitmix32 = (a: number) => {
+  return () => {
+    a |= 0;
+    a = (a + 0x9e3779b9) | 0;
+    let t = a ^ (a >>> 16);
+    t = Math.imul(t, 0x21f0aaad);
+    t = t ^ (t >>> 15);
+    t = Math.imul(t, 0x735a2d97);
+    return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
+  };
+};
