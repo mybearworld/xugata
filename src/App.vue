ea9371739d40cc143b30a26cb737419f37494dcd
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-05-10 14:03:21 +0200

    Get input from body listeners

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 import { currentWord } from "./lib/currentWord.ts";
-import { type Evaluation, newGame } from "./lib/game.ts";
+import { type Evaluation, newGame, WORD_LENGTH } from "./lib/game.ts";
 import GameGrid from "./components/GameGrid.vue";
+
+const BATELU_LETTER = /^[a-pr-z]$/;
 
 const game = ref(newGame(currentWord()));
 const guesses = ref<Evaluation[]>([]);
 const input = ref("");
 
-const submit = (e: SubmitEvent) => {
-  e.preventDefault();
+const makeGuess = () => {
   const guess = game.value.guess(input.value);
   if (guess.error) {
     alert(guess.msg);
@@ -18,12 +19,30 @@ const submit = (e: SubmitEvent) => {
   guesses.value.push(guess.evaluation);
   input.value = "";
 };
+
+const keydownEventListener = (e: KeyboardEvent) => {
+  switch (e.key) {
+    case "Backspace":
+      e.preventDefault();
+      input.value = input.value.slice(0, -1);
+      return;
+    case "Enter":
+      makeGuess();
+      break;
+    default:
+      if (!BATELU_LETTER.test(e.key)) return;
+      if (input.value.length === WORD_LENGTH) return;
+      input.value += e.key;
+  }
+};
+onMounted(() => {
+  document.body.addEventListener("keydown", keydownEventListener);
+});
+onBeforeUnmount(() => {
+  document.body.removeEventListener("keydown", keydownEventListener);
+});
 </script>
 
 <template>
-  <form @submit="submit">
-    <input class="border" type="text" v-model="input" />
-    <button>Submit</button>
-  </form>
   <GameGrid :evaluations="guesses" :input />
 </template>
