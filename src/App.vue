e1c6ce7de44c353e3257667d6a4155b2086c6c61
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-05-10 14:52:55 +0200

    Add keyboard for mobile

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import {
   WORD_LENGTH,
 } from "./lib/game.ts";
 import GameGrid from "./components/GameGrid.vue";
+import MobileKeyboard from "./components/MobileKeyboard.vue";
 
 const BATELU_LETTER = /^[a-pr-z]$/;
 
@@ -27,20 +28,27 @@ const makeGuess = () => {
   input.value = "";
 };
 
+const addLetter = (letter: string) => {
+  if (!BATELU_LETTER.test(letter)) return;
+  if (input.value.length === WORD_LENGTH) return;
+  input.value += letter;
+};
+const backspace = () => {
+  input.value = input.value.slice(0, -1);
+};
+
 const keydownEventListener = (e: KeyboardEvent) => {
   if (gameState.value !== "ongoing") return;
   switch (e.key) {
     case "Backspace":
       e.preventDefault();
-      input.value = input.value.slice(0, -1);
+      backspace();
       return;
     case "Enter":
       makeGuess();
       break;
     default:
-      if (!BATELU_LETTER.test(e.key)) return;
-      if (input.value.length === WORD_LENGTH) return;
-      input.value += e.key;
+      addLetter(e.key);
   }
 };
 onMounted(() => {
@@ -55,5 +63,10 @@ onBeforeUnmount(() => {
   <div class="flex h-screen flex-col items-center justify-center gap-5">
     <h1 class="text-2xl">xuga wordyl</h1>
     <GameGrid :evaluations="guesses" :input />
+    <MobileKeyboard
+      @type="addLetter"
+      @backspace="backspace"
+      @submit="makeGuess"
+    />
   </div>
 </template>
---
+++ src/components/MobileKeyboard.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  type: [letter: string];
+  backspace: [];
+  submit: [];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-2">
+    <div class="flex gap-2">
+      <button
+        class="flex h-10 w-10 cursor-pointer items-center justify-center bg-stone-600 text-xl uppercase"
+        @click="() => emit('type', letter)"
+        v-for="letter in 'qwertyuiop'"
+      >
+        {{ letter }}
+      </button>
+    </div>
+    <div class="flex gap-2">
+      <button
+        class="flex h-10 w-10 cursor-pointer items-center justify-center bg-stone-600 text-xl uppercase"
+        @click="() => emit('type', letter)"
+        v-for="letter in 'asdfghjkl'"
+      >
+        {{ letter }}
+      </button>
+    </div>
+    <div class="flex gap-2">
+      <button
+        class="flex h-10 w-10 cursor-pointer items-center justify-center bg-stone-600 text-xl uppercase"
+        @click="emit('backspace')"
+        aria-label="Backspace"
+      >
+        <!-- Tabler icons; MIT -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M20 6a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-11l-5 -5a1.5 1.5 0 0 1 0 -2l5 -5l11 0"
+          />
+          <path d="M12 10l4 4m0 -4l-4 4" />
+        </svg>
+      </button>
+      <button
+        class="flex h-10 w-10 cursor-pointer items-center justify-center bg-stone-600 text-xl uppercase"
+        @click="() => emit('type', letter)"
+        v-for="letter in 'zxcvbnm'"
+      >
+        {{ letter }}
+      </button>
+      <button
+        class="flex h-10 w-10 cursor-pointer items-center justify-center bg-stone-600 text-xl uppercase"
+        @click="() => emit('submit')"
+        aria-label="Submit"
+      >
+        <!-- Tabler icons; MIT -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M18 6v6a3 3 0 0 1 -3 3h-10l4 -4m0 8l-4 -4" />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
